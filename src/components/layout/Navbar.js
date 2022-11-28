26014fd274da28f2fffe6b2fdf4b6c7c28bf3d19
Edwin Wanjuki <edwinwanjuki@gmail.com>
2022-11-28 03:21:23 +0300

    footer

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,15 +6,21 @@ function Navbar() {
     <nav className="navbar mb-12 bg-neutral text-neutral-content shadow-lg">
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
-            <FaGithub className="text-3xl inline pr-2"/>
-            <Link to="/" className="font-bold text-lg align-middle">Github Finder</Link>
+          <FaGithub className="text-3xl inline pr-2" />
+          <Link to="/" className="font-bold text-lg align-middle">
+            Github Finder
+          </Link>
         </div>
 
         <div className="flex-1 px-2 mx-2">
-            <div className="flex justify-end">
-                <Link to="/" className="btn btn-ghost btn-sm rounded-btn">Home</Link>
-                <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">About</Link>
-            </div>
+          <div className="flex justify-end">
+            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+              Home
+            </Link>
+            <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
+              About
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
