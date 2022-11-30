cb9393bb7987f1e0192986a7ca86f5f78d918874
Edwin Wanjuki <edwinwanjuki@gmail.com>
2022-12-01 02:53:38 +0300

    UserResults & Spinner components

---
+++ src/components/users/UserResults.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import Spinner from "../UI/Spinner";
+
+function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_GITHUB_API_URL}/users`,
+      {
+        headers: {
+          Authorization: `token ${process.env.REACT_APP_GITHUB_API_TOKEN}`,
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    setUsers(data);
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <Spinner />
+  }
+
+  return (
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+      {users.map((user) => (
+        <h3 key={user.id}>{user.login}</h3>
+      ))}
+    </div>
+  );
+}
+
+export default UserResults;
