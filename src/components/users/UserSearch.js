91159c9476e87b393135252866608edae1c37081
Edwin Wanjuki <edwinwanjuki@gmail.com>
2022-12-01 16:29:58 +0300

    user search component

---
+++ src/components/users/UserSearch.js
@@ -0,0 +1,52 @@
+import { useState, useContext } from "react";
+import GithubContext from "../../store/github/github-context";
+
+function UserSearch() {
+  const [text, setText] = useState("");
+  const { users } = useContext(GithubContext);
+
+  const textChangeHandler = (e) => setText(e.target.value);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (text === "") {
+      return alert("Please enter something.");
+    }
+
+    // @todo - Search Users
+
+    setText("");
+  };
+
+  return (
+    <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2">
+      <form onSubmit={submitHandler}>
+        <div className="form-control">
+          <div className="relative">
+            <input
+              type="text"
+              className="w-full pr-40 bg-gray-200 input input-lg text-black"
+              placeholder="Search"
+              value={text}
+              onChange={textChangeHandler}
+            />
+            <button
+              type="submit"
+              className="btn btn-lg absolute top-0 right-0 rounded-l-none w-36"
+            >
+              Go
+            </button>
+          </div>
+        </div>
+      </form>
+      {users.length > 0 && (
+        <div>
+          <button className="btn btn-ghost btn-lg">CLEAR</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default UserSearch;
